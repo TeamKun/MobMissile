b29f38dd9dc65d5f37c568fe50bfceb145a8866a
maru-256 <maru043@docomo.ne.jp>
2021-07-18 01:56:59 +0900

    first commit

---
+++ src/main/java/net/kunmc/lab/mobmissile/MobMissile.java
@@ -0,0 +1,14 @@
+package net.kunmc.lab.mobmissile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobMissile extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
